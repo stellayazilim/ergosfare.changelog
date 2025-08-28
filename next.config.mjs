41c99b6fa1b45f0a1305de11416d467d7d61e34b
Kerim Cetinbas <a.cetinbas@icloud.com>
2025-08-28 04:20:01 +0300

    build site

--- next.config.mjs
+++ next.config.mjs
@@ -16,6 +16,7 @@ const withMDX = nextMDX({
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  distDir: 'docs',
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'],
 }
 
