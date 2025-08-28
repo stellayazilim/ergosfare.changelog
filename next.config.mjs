43ba0d13847e215da42b102f112cd49d66bb5951
Kerim Cetinbas <a.cetinbas@icloud.com>
2025-08-28 04:55:16 +0300

    next.config

--- next.config.mjs
+++ next.config.mjs
@@ -16,6 +16,7 @@ const withMDX = nextMDX({
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  distDir: 'docs',
   basePath: '/ergosfare.changelog',
   assetPrefix: 'https://stellayazilim.github.io/ergosfare.changelog/',
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'],
